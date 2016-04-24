b3ba0f1801bdb9e634d0cbf77ef18c0bb6ec4dd5
tyeah <ye.tian.stanford@gmail.com>
2016-04-24 02:14:11 -0700

    (tensor, shape) in, (tensor, shape) out

--- neuralcraft/layers.py
+++ neuralcraft/layers.py
@@ -1,11 +1,9 @@
 '''
 To use this module, one need to define input tensors and an empty dictionary params = {}
-Every layer function receives tensor expressions, params and param names and layer 
+Every layer function receives a tuple of (tensor expressions, input shape), params and param names and layer 
 definition options defines shared variables and places them in params. return another 
-tensor expression(s)
+(tensor expression(s), output_shape)
 '''
-# TODO: output the shape of output at each layer and also offer as input to the next layer, 
-# to infer weight shape
 import theano
 from theano import tensor as T
 from theano.tensor import nnet
@@ -31,23 +29,31 @@ def add_param(shape, params, name=None, val=None):
   params[name] = theano.shared(np.asarray(val, dtype=theano.config.floatX))
   return name
 
-def FCLayer(incoming, params, num_in, num_out, activation=nnet.relu, 
+def FCLayer(incoming, params, num_out, activation=nnet.relu, 
     w_name=None, b_name=None, w=None, b=None):
+  incoming, input_shape = incoming
+  num_in = np.prod(input_shape[1:])
+
+  output_shape = (input_shape[0], num_out)
   w_name = name_suf(w_name, 'W_fc')
   b_name = name_suf(b_name, 'b_fc')
   w_name = add_param((num_in, num_out), params, w_name, w)
   b_name = add_param((num_out,), params, b_name, b)
   if incoming.ndim > 2:
     incoming = incoming.flatten(2)
-  return activation(incoming.dot(params[w_name]) + params[b_name])
+  return (activation(incoming.dot(params[w_name]) + params[b_name]), output_shape)
 
-def Conv2DLayer(incoming, params, num_in, num_out, filter_h, filter_w=None, filter=None, filter_name=None,
+
+def Conv2DLayer(incoming, params, num_out, filter_h, filter_w=None, filter=None, filter_name=None,
     stride_h=None, stride_w=None, padding='half', activation=nnet.relu):
   '''
   incoming shoule be a tensor4: (batch_size, channel_size, height, width)
   filter should be None or ndarray or shared
   here num_in == channel_size. how to infer automatically?
-  '''
+  ''' 
+  incoming, input_shape = incoming
+  num_in, input_h, input_w = input_shape[-3:]
+
   assert filter_h % 2 == 1
   if not filter_w:
     filter_w = filter_h
@@ -62,12 +68,65 @@ def Conv2DLayer(incoming, params, num_in, num_out, filter_h, filter_w=None, filt
       filter.get_value().shape==(num_out, incoming.shape[1], filter_h, filter_w))
   filter_name = add_param((num_out, num_in, filter_h, filter_w), 
       params, filter_name, filter)
-  return activation(nnet.conv2d(incoming, params[filter_name], border_mode=padding, 
+  if padding == 'half':
+    output_h, output_w = input_h, input_w
+  else:
+    raise NotImplementedError("not implemented output shape for padding patterns other than 'half'")
+  output_shape = (input_shape[0], num_out, output_h, output_w)
+  output = activation(nnet.conv2d(incoming, params[filter_name], border_mode=padding, 
     subsample=(stride_h, stride_w)))
+  return (output, output_shape)
 
 
-def RNNLayer(incoming, hid_init, params, num_in, num_hidden, activation=nnet.relu, 
+def dropoutLayer(incoming, use_noise, trng, p):
+  """
+  tensor switch is like an if statement that checks the
+  value of the theano shared variable (use_noise) (we can also use 0/1), 
+  before either dropping out the incoming tensor or
+  computing the appropriate activation. During training/testing
+  use_noise is toggled on and off.
+  """
+  incoming, input_shape = incoming
+  output_shape = input_shape
+  proj = T.switch(use_noise,
+                  incoming *
+                  trng.binomial(incoming.shape, p=p, n=1, dtype=incoming.dtype),
+                  incoming * p)
+  return (proj, output_shape)
+
+
+def poolingLayer(incoming, ds_h, ds_w=None, stride_h=None, stride_w=None, padding=0, mode='max'):
+  incoming, input_shape = incoming
+  input_h, input_w = input_shape[-2:]
+  if not ds_w:
+    ds_w = ds_h
+  ds = (ds_w, ds_h)
+  if not stride_h and not stride_w:
+    st = ds
+  elif stride_h != None and stride_w != None:
+    st = (stride_h, stride_w)
+  else:
+    st = stride_h if stride != None else stride_w
+    st = (st, st)
+  if isinstance(padding, int):
+    padding = (padding, padding)
+  elif isinstance(padding, tuple):
+    assert len(padding) == 2
+  output = T.signal.pool.pool_2d(incoming, ds, ignore_border=False, st=st, padding=padding, mode=mode)
+  output_h = input_h + 2 * padding[0]
+  output_h = int(output_h / st[0])
+  output_w = input_w + 2 * padding[1]
+  output_w = int(output_w / st[1])
+  output_shape = tuple(list(input_shape[:-2]) + [output_h, output_w])
+  return output, output_shape
+
+
+def RNNLayer(incoming, hid_init, params, num_hidden, activation=nnet.relu, 
     w_xh_name=None, w_hh_name=None, b_name=None, w_xh=None, w_hh=None, b=None):
+  incoming, input_shape = incoming
+  num_in = input_shape[-1]
+  output_shape = (input_shape[0], input_shape[1], num_hidden)
+
   rnnwxh_name = add_param((num_in, num_hidden), params, w_xh_name, w_xh)
   rnnwhh_name = add_param((num_hidden, num_hidden), params, w_hh_name, w_hh)
   rnnb_name = add_param((num_hidden, ), params, b_name, b)
@@ -87,41 +146,10 @@ def RNNLayer(incoming, hid_init, params, num_in, num_hidden, activation=nnet.rel
       outputs_info=[{'initial':hid_init, 'taps':[-1]}],
       sequences=[incoming.dimshuffle((1, 0, 2))],
       n_steps=incoming.shape[1])
-  return results.dimshuffle((1, 0, 2))
-
-def dropoutLayer(incoming, use_noise, trng, p):
-  """
-  tensor switch is like an if statement that checks the
-  value of the theano shared variable (use_noise) (we can also use 0/1), 
-  before either dropping out the incoming tensor or
-  computing the appropriate activation. During training/testing
-  use_noise is toggled on and off.
-  """
-  proj = T.switch(use_noise,
-                  incoming *
-                  trng.binomial(incoming.shape, p=p, n=1, dtype=incoming.dtype),
-                  incoming * p)
-  return proj
-
-def poolingLayer(incoming, ds_h, ds_w=None, stride_h=None, stride_w=None, padding=0, mode='max'):
-  if not ds_w:
-    ds_w = ds_h
-  ds = (ds_w, ds_h)
-  if not stride_h and not stride_w:
-    st = None
-  elif stride_h != None and stride_w != None:
-    st = (stride_h, stride_w)
-  else:
-    st = stride_h if stride != None else stride_w
-    st = (st, st)
-  if isinstance(padding, int):
-    padding = (padding, padding)
-  elif isinstance(padding, tuple):
-    assert len(padding) == 2
-  return T.signal.pool.pool_2d(incoming, ds, ignore_border=False, st=st, padding=padding, mode=mode)
+  return (results.dimshuffle((1, 0, 2)), output_shape)
 
 
-def LSTMLayer(incoming, hid_init, cell_init, params, num_in, num_hidden, activation=T.tanh, gate_act=nnet.sigmoid,
+def LSTMLayer(incoming, cell_init, hid_init, params, num_hidden, activation=T.tanh, gate_act=nnet.sigmoid,
     w_xi_name=None, w_hi_name=None, b_i_name=None, w_xi=None, w_hi=None, b_i=None,
     w_xf_name=None, w_hf_name=None, b_f_name=None, w_xf=None, w_hf=None, b_f=None,
     w_xo_name=None, w_ho_name=None, b_o_name=None, w_xo=None, w_ho=None, b_o=None,
@@ -129,6 +157,10 @@ def LSTMLayer(incoming, hid_init, cell_init, params, num_in, num_hidden, activat
   '''
   hid_init and cell_init can be a number, an array or a tensor expression
   '''
+  incoming, input_shape = incoming
+  num_in = input_shape[-1]
+  output_shape = (input_shape[0], input_shape[1], num_hidden)
+
   # add parameters
   wxi_name = add_param((num_in, num_hidden), params, w_xi_name, w_xi)
   whi_name = add_param((num_hidden, num_hidden), params, w_hi_name, w_hi)
@@ -174,7 +206,7 @@ def LSTMLayer(incoming, hid_init, cell_init, params, num_in, num_hidden, activat
       sequences=[incoming.dimshuffle((1, 0, 2))],
       n_steps=incoming.shape[1])
   hid_state, cell_stat = results[0].dimshuffle((1, 0, 2)), results[1].dimshuffle((1, 0, 2))
-  return hid_state
+  return (hid_state, output_shape)
 
 
 def cross_entropy(yhat, y):
--- neuralcraft/test.py
+++ neuralcraft/test.py
@@ -8,7 +8,7 @@ seq_size = 5
 x_size = 3
 hid_size = 4
 output_size = 2
-num_iter = 10
+num_iter = 2
 filter_size = 3
 channel_size = 3
 hid_channel_size = 6
@@ -17,64 +17,72 @@ dropout_p = 0.4
 trng = RandomStreams(1234)
 
 def FCTest():
+  x = np.random.randn(batch_size, x_size)
+  y = np.random.rand(batch_size) > 0.5
+
   xt = T.matrix()
+  xt = (xt, x.shape)
   yt = T.ivector()
   params = {}
-  xout = FCLayer(xt, params, x_size, output_size)
-  yhatt = T.nnet.softmax(xout)
+  xout = FCLayer(xt, params, output_size)
+  yhatt = T.nnet.softmax(xout[0])
   loss = T.mean(T.nnet.categorical_crossentropy(yhatt, yt))
   updates = sgd(loss, params)
 
-  f = theano.function([xt, yt], loss, updates = updates, allow_input_downcast=True) 
+  f = theano.function([xt[0], yt], loss, updates = updates, allow_input_downcast=True) 
 
-  x = np.random.randn(batch_size, x_size)
-  y = np.random.rand(batch_size) > 0.5
   for i in range(num_iter):
     print f(x, y)
     print [v.get_value() for v in params.values()]
 
 def RNNTest():
-  xt = T.tensor3()
-  hidt = T.matrix()
-  yt = T.imatrix()
-  params = {}
-  rnnout = RNNLayer(xt, hidt, params, x_size, hid_size)
-  rnnout = rnnout.reshape((-1, rnnout.shape[-1]))
-  # neet to reshape flatten first 2 dim of rnn output before inputing to FCLayer
-  yhatt = FCLayer(rnnout, params, hid_size, output_size, activation=T.nnet.softmax)
-  loss = T.mean(T.nnet.categorical_crossentropy(yhatt, yt.flatten()))
-  updates = sgd(loss, params)
-
-  f = theano.function([xt, hidt, yt], loss, updates = updates, allow_input_downcast=True) 
-
   x = np.random.randn(batch_size, seq_size, x_size)
   hid = np.random.randn(batch_size, hid_size)
   y = np.random.rand(batch_size, seq_size) > 0.5
+
+  xt = T.tensor3()
+  xt = (xt, x.shape)
+  hidt = T.matrix()
+  yt = T.imatrix()
+  params = {}
+  rnnout, rnnout_shape = RNNLayer(xt, hidt, params, hid_size)
+  rnnout = rnnout.reshape((-1, rnnout.shape[-1]))
+  rnnout_shape = (np.prod(rnnout_shape[-1:]), rnnout_shape[-1])
+  # neet to reshape flatten first 2 dim of rnn output before inputing to FCLayer
+  yhatt, _ = FCLayer((rnnout, rnnout_shape), params, output_size, activation=T.nnet.softmax)
+  loss = T.mean(T.nnet.categorical_crossentropy(yhatt, yt.flatten()))
+  updates = sgd(loss, params)
+
+  f = theano.function([xt[0], hidt, yt], loss, updates = updates, allow_input_downcast=True) 
+
   for i in range(num_iter):
     print f(x, hid, y)
-    print [v.get_value() for v in params.values()]
+    #print [v.get_value() for v in params.values()]
 
 def LSTMTest():
+  x = np.random.randn(batch_size, seq_size, x_size)
+  hid = np.random.randn(batch_size, hid_size)
+  cell = np.random.randn(batch_size, hid_size)
+  y = np.random.rand(batch_size, seq_size) > 0.5
+
   xt = T.tensor3()
+  xt = (xt, x.shape)
   hidt = T.matrix()
   cellt = T.matrix()
   yt = T.imatrix()
   params = {}
   #rnnout = LSTMLayer(xt, hidt, cellt, params, x_size, hid_size)
-  rnnout = LSTMLayer(xt, 0, np.random.randn(hid_size), params, x_size, hid_size)
+  rnnout, rnnout_shape = LSTMLayer(xt, 0, np.random.randn(hid_size), params, hid_size)
   rnnout = rnnout.reshape((-1, rnnout.shape[-1]))
+  rnnout_shape = (np.prod(rnnout_shape[-1:]), rnnout_shape[-1])
   # neet to reshape flatten first 2 dim of rnn output before inputing to FCLayer
-  yhatt = FCLayer(rnnout, params, hid_size, output_size, activation=T.nnet.softmax)
+  yhatt, _ = FCLayer((rnnout, rnnout_shape), params, output_size, activation=T.nnet.softmax)
   loss = T.mean(T.nnet.categorical_crossentropy(yhatt, yt.flatten()))
   updates = sgd(loss, params)
 
   #f = theano.function([xt, hidt, cellt, yt], loss, updates = updates, allow_input_downcast=True) 
-  f = theano.function([xt, yt], loss, updates = updates, allow_input_downcast=True) 
+  f = theano.function([xt[0], yt], loss, updates = updates, allow_input_downcast=True) 
 
-  x = np.random.randn(batch_size, seq_size, x_size)
-  hid = np.random.randn(batch_size, hid_size)
-  cell = np.random.randn(batch_size, hid_size)
-  y = np.random.rand(batch_size, seq_size) > 0.5
   for i in range(num_iter):
     #print f(x, hid, cell, y)
     print f(x, y)
@@ -85,19 +93,20 @@ def Conv2DTest():
   y = np.random.rand(batch_size) > 0.5
 
   xt = T.tensor4()
+  xt = (xt, x.shape)
   yt = T.ivector()
   params = {}
-  cnnout = Conv2DLayer(xt, params, channel_size, hid_channel_size, filter_size)
-  f = theano.function([xt], cnnout, allow_input_downcast=True) 
-  print f(x).shape
+  cnnout = Conv2DLayer(xt, params, hid_channel_size, filter_size)
+  f = theano.function([xt[0]], cnnout[0], allow_input_downcast=True) 
+  print f(x).shape, cnnout[1]
 
 
-  yhatt = FCLayer(cnnout, params, x_size * x_size * hid_channel_size, output_size, activation=T.nnet.softmax)
+  yhatt, _ = FCLayer(cnnout, params, output_size, activation=T.nnet.softmax)
   loss = T.mean(T.nnet.categorical_crossentropy(yhatt, yt.flatten()))
   updates = sgd(loss, params)
 
   #f = theano.function([xt, hidt, cellt, yt], loss, updates = updates, allow_input_downcast=True) 
-  f = theano.function([xt, yt], loss, updates = updates, allow_input_downcast=True) 
+  f = theano.function([xt[0], yt], loss, updates = updates, allow_input_downcast=True) 
 
   for i in range(num_iter):
     #print f(x, hid, cell, y)
@@ -105,33 +114,38 @@ def Conv2DTest():
     print [v.get_value() for v in params.values()]
 
 def dropoutTest():
+  x = np.random.randn(batch_size, seq_size, x_size)
+
   xt = T.tensor3()
+  xt = (xt, x.shape)
   use_noise = T.bscalar()
-  yt = dropoutLayer(xt, use_noise, trng, dropout_p)
-  f = theano.function([xt, use_noise], yt, allow_input_downcast=True) 
+  yt, _ = dropoutLayer(xt, use_noise, trng, dropout_p)
+  f = theano.function([xt[0], use_noise], yt, allow_input_downcast=True) 
 
   x = np.random.randn(batch_size, seq_size, x_size)
   print f(x, True)
 
   use_noise = 1
-  yt = dropoutLayer(xt, use_noise, trng, dropout_p)
-  f = theano.function([xt], yt, allow_input_downcast=True) 
+  yt, _ = dropoutLayer(xt, use_noise, trng, dropout_p)
+  f = theano.function([xt[0]], yt, allow_input_downcast=True) 
 
-  x = np.random.randn(batch_size, seq_size, x_size)
   print f(x)
 
 def poolingTest():
-  xt = T.tensor3()
-  yt = poolingLayer(xt, ds_h=2)
-  f = theano.function([xt], yt, allow_input_downcast=True) 
-
   x = np.random.randn(batch_size, 10, 10)
+
+  xt = T.tensor3()
+  xt = (xt, x.shape)
+  yt, pool_shape = poolingLayer(xt, ds_h=2)
+  f = theano.function([xt[0]], yt, allow_input_downcast=True) 
+
+  print pool_shape
   print f(x)
 
 if __name__ == '__main__':
   #FCTest()
   #RNNTest()
   #LSTMTest()
-  #Conv2DTest()
+  Conv2DTest()
   #dropoutTest()
-  poolingTest()
+  #poolingTest()
